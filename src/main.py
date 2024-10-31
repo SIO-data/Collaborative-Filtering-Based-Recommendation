42f1d598de76d91ba352cd62c2b139f55f886f9b
Thomas Chourret <chourret.thomas@gmail.com>
2024-10-31 16:39:42 +0100

    updated code

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
-input_file = '../assets/recommendation_dataset.csv'
-
-df = df[:, 1:]
+input_file = 'recommendation_dataset.csv'
 
 dfp = pd.read_csv(input_file, sep=';', skiprows=1)
 dfp = dfp.iloc[:, 1:].replace(99.0, None)
 
 print(dfp)
-print(dfp.mean(axis=None, skipna=True))
+mean = dfp.mean(axis=None, skipna=True)
+
+dfp = dfp.fillna(mean)
+print(dfp)
+
